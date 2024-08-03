73c7b8040d7636fcc8453a0f78e227d0528b738b
Pranjal Pruthi <47497714+pranjalpruthi@users.noreply.github.com>
2024-08-04 01:36:03 +0530

    Update 1_manage.py

--- pages/1_manage.py
+++ pages/1_manage.py
@@ -1,3 +1,4 @@
+import hmac
 import streamlit as st
 import psycopg2
 
@@ -63,12 +64,39 @@ def remove_report(id):
         if conn:
             conn.close()
 
+# Password check function
+def check_password():
+    """Returns True if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
 # Streamlit app
 st.set_page_config(
     page_title="Manage Devotees",
     page_icon="🙏",
     layout='wide',
 )
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 st.title('Manage Devotees')
 
