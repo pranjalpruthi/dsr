02207442d585c3f25d70a20509a8267a9307dd60
Pranjal Pruthi <47497714+pranjalpruthi@users.noreply.github.com>
2024-08-04 01:43:00 +0530

    Update 1_manage.py

--- pages/1_manage.py
+++ pages/1_manage.py
@@ -64,13 +64,12 @@ def remove_report(id):
         if conn:
             conn.close()
 
-# Password check function
 def check_password():
     """Returns True if the user had the correct password."""
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]["password"]):
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password.
         else:
